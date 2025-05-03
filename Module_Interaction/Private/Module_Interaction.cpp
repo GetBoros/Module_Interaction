3588357cf06bd2cf4883c0ddebdf006d934217ca
Andrey <getboros@gmail.com>
2025-05-03 09:39:20 +0300

    refactoring, backup

--- Module_Interaction/Private/Module_Interaction.cpp
+++ Module_Interaction/Private/Module_Interaction.cpp
@@ -1,6 +1,5 @@
 #include "Module_Interaction.h"
 #include "Modules/ModuleManager.h"
 
-// def immplement  makes available to c++ module
 IMPLEMENT_MODULE ( FDefaultModuleImpl, Module_Interaction);
 
