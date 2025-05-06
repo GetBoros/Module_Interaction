d0c94b64d3d3469ff7a7573fe785defeb6b6fef5
Andrey <getboros@gmail.com>
2025-05-06 16:39:31 +0300

    backup, test

--- Module_Interaction/Public/Module_Interaction_Component.h
+++ Module_Interaction/Public/Module_Interaction_Component.h
@@ -4,6 +4,19 @@
 #include "Module_Interaction_Component.generated.h"
 
 //-------------------------------------------------------------------------------------------------------------
+UCLASS(BlueprintType) class UItemDataAsset : public UPrimaryDataAsset
+{
+    GENERATED_BODY()
+
+public:
+    UPROPERTY(EditDefaultsOnly) FText item_name;
+    UPROPERTY(EditDefaultsOnly) UTexture2D *icon;
+    //UPROPERTY(EditDefaultsOnly) TSubclassOf<UGameplayAbility> use_ability_class;
+    UPROPERTY(EditDefaultsOnly) bool is_stackable;
+    UPROPERTY(EditDefaultsOnly) int32 max_stack;
+};
+
+//-------------------------------------------------------------------------------------------------------------
 UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent)) class MODULE_INTERACTION_API UAModule_Interaction_Component : public UActorComponent
 {
 	GENERATED_BODY()
